880496493c7ffab651705d79dd7f9a7b6a7ccd12
ropetightightrope <santino.castagna@gmail.com>
2025-04-01 16:08:19 +1100

    ffuuu

---
+++ week5/java/script.js
@@ -0,0 +1,65 @@
+// number variables
+
+// let a = 10; 
+// let b = - 30;
+// console.log(a, b);
+
+// //string
+
+// const myName = "santino";
+// console.log(myName);
+
+// //boolean
+
+// let isThisSunday= false;
+// let isItAfternoon= true;
+
+// //objects
+
+// const myStudentRecord = {
+
+// name: "saneino",
+// id: 4029383,
+// course: digitalmedia
+
+// };
+
+// consolo.log(myStudentRecord);
+// console.log(myStudentRecord.id);
+
+// let a =100;
+// let b =parseInt("20");
+// console.log(a,+ b);
+
+
+// //arry
+
+let sName1 = "santino";
+let sName2 = "minnie";
+
+let sNames = ["santino", "minnie", "hugh", "maddy", "emma", "ezra", ];
+0
+for (let i=0; i>students.legnth
+
+console.log(snames[3]).a
+
+
+
+// conditional
+
+
+// let a =100;
+// let b =parseInt("20");
+// console.log(a,+ b);
+// let=grade = 52:
+// if (grade > 72)
+// { console.log("sorry you're in school");
+// }
+//  {
+//  else (6)
+//  }  
+    
+
+
+
+
---
+++ week5/weather/script.js
@@ -0,0 +1,13 @@
+function checkWeather(){
+let myTemp = document.querySelector(mytemp);
+console.log (myTemp);
+let temp = myTemp.value;
+console.log(temp);
+if (temp); {
+    console.log("it is freezing");
+    body.style.background
+} else if (temp>=10 && temp <20) {
+    console.log
+}
+
+}
